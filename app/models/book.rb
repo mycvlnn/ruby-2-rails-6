1daaf0f2a2c72940e6cafd2eb3f90d9a13d07190
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2025-08-27 22:37:41 +0700

    feat: dump data

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class Book < ApplicationRecord
   belongs_to :author
+  has_many :orders
+  has_many :customers, through: :orders
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Customer < ApplicationRecord
+  has_many :orders
+  has_many :books, through: :orders
 end
