0aff9cd30170332a5ec8298ec782be1d5e97fda1
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2025-09-23 17:58:30 +0700

    feat: Association callbacks

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class Author < ApplicationRecord
-  has_many :books, -> { order(year_published: :asc)}
-
+  has_many :books, -> { order(year_published: :asc) }, before_add: :check_credit_limit
   validates :name, presence: true
+
+  def check_credit_limit(book)
+    throw(:abort) if limit_reached?
+  end
+
+  def limit_reached?
+    books.count >= 8
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -6,7 +6,6 @@ class Book < ApplicationRecord
   has_many :customers, through: :orders
   has_many :reviews, dependent: :destroy
 
-
   scope :costs_more_than, ->(amount) { where('price > ?', amount) if amount.present? }
   scope :recent, -> { where(year_published: 10.years.ago.year..) }
   scope :in_print, -> { where(out_of_print: false) }
